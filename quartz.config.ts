ee81d8e8324d53d0b219b2d8b842bb92c1992623
ashwinprakashsolanki-coder <ashwin.prakash.solanki@gmail.com>
2026-04-07 00:11:09 +0530

    Fix 2

--- quartz.config.ts
+++ quartz.config.ts
@@ -81,7 +81,7 @@ const config: QuartzConfig = {
       Plugin.FolderPage(),
       Plugin.TagPage(),
       Plugin.ContentIndex({
-      enableSiteMap: true,
+      // enableSiteMap: true,
       //  enableRSS: true,
       }),
       Plugin.Assets(),
