f72d64d0edd9452bab09694b228de64c84e0d5ea
the-mortalbard <ashwinsolanki083@gmail.com>
2026-07-14 15:14:06 +0530

    changing name

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Mortal's Thots",
+    pageTitle: "Mortal's Thoughts",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
